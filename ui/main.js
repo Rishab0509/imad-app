a61f758507df0dffd624fbddced9b5e23d97e3d8
Rishab0509 <30807377+Rishab0509@users.noreply.github.com>
2017-08-28 18:50:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,12 +3,12 @@ console.log('Loaded!');
 
 var img = document.getElementById('madi');
 
-var newlocation = 0;
+var marginLeft = 0;
 
 var animate = function()
 {
     newlocation += 1;
-    img.css.marginLeft = newlocation + 'px';
+    img.css.marginLeft = marginLeft + 'px';
 };
 
 img.onclick = function()
