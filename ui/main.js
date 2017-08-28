56fc52359ecc6153d5ae858be5fb57463d7a8cd0
Rishab0509 <30807377+Rishab0509@users.noreply.github.com>
2017-08-28 20:35:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,26 @@ img.onclick = function()
 {
     setInterval(animate , 50);
 };
+
+
+
+// Counter Code
+
+var xhttp = require('XMLHttpRequest');
+
+xhttp.onreadystatechange = function(){
+  
+  if(xhttp.readyState == 4)
+  {
+      if(xhttp.status == 200)
+      {
+          var counter = xhttp.responseText;
+          var span = document.getElementById('span');
+          span.innerHtml = counter;
+      }
+  }
+  
+};
+
+xhttp.open('GET' , 'http://rshbbamrara.imad.hasura-app.io/counter' , true);
+xhttp.send();
