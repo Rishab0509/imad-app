5294576f83baa5cbff39783e62fed938b9daf3de
Rishab0509 <30807377+Rishab0509@users.noreply.github.com>
2017-08-28 17:50:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,91 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    
+    'article-one' : {
+        
+        title : 'This is Article one' ,
+        heading : ' This is Article One', 
+        date : '28 Aug 2017' , 
+        para : ` <p> This is very first paragraph <br/> Please like it</p>
+                <p> This is the second paragraph <br/> Make sure to like it as well
+                </p>`
+    } ,
+    
+    'article-two' : {
+        
+        title : 'This is Article two' ,
+        heading : ' This is Article Two', 
+        date : '28 Aug 2017' , 
+        para : ` <p> This is very first paragraph <br/> Please like it</p>
+                <p> This is the second paragraph <br/> Make sure to like it as well
+                </p>`
+    } ,
+    
+    'article-three' : {
+        
+        title : 'This is Article three' ,
+        heading : ' This is Article Three', 
+        date : '28 Aug 2017' , 
+        para : ` <p> This is very first paragraph <br/> Please like it</p>
+                <p> This is the second paragraph <br/> Make sure to like it as well
+                </p>`
+    }
+    
+};
+
+var createTemplate = function(data){
+
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var para = data.para;
+    
+    var template = 
+     `<html>
+        <head> 
+          <title> 
+            ${title}
+          </title> 
+          <meta name='viewport' content="width = device-width , initial-scale =1 " />
+          <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        
+        <body>
+            <div class = "container">
+                <div>
+                    <h1>${heading}</h1>
+                </div>
+                <hr/>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${para}
+                </div>
+            </div>    
+        </body>
+        
+    </html>`;
+
+    return template;
+} ;   
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one' , function(req,res){
+
+app.get('/:article' , function(req,res) {
+    
+   var article-name = req.params.article ;
+   
+});
+
+
+/*app.get('/article-one' , function(req,res){
    
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
     
@@ -25,7 +105,7 @@ app.get('/article-three' , function(req,res){
    
    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
     
-});
+});*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
