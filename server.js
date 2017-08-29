599b803986b0936fb9d139eb5ff04d6d2c4010b6
Rishab0509 <30807377+Rishab0509@users.noreply.github.com>
2017-08-29 14:22:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/', function (req, res) {
 
 app.get('/test-db' , function(req,res){
     
-    pool.query('SELECT * FROM ARTICLES' ,  function(err,result){
+    pool.query('SELECT * FROM "ARTICLES"' ,  function(err,result){
        
        if(err){
            res.status(500).send(err.toString());
